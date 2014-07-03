d718653601d3f8f876d1fcbe7ebb3a7ccc049c82
Bill DePhillips <bill.dephillips@gmail.com>
2014-07-03 09:42:35 -0700

    removed unnecessary code

--- src/main/java/com/thoughtworks/mingle/murmurs/Author.java
+++ src/main/java/com/thoughtworks/mingle/murmurs/Author.java
@@ -7,14 +7,6 @@ public class Author {
 
     private String name;
     private String icon_url;
-//  private String login;
-//  private String email;
-//  private boolean light;
-//  private String icon_path;
-//  private boolean activated;
-//  private boolean admin;
-//  private String version_control_user_name;
-//  private String jabber_user_name;
 
     public String getName() {
         return name;
